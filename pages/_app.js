52a234790486c288cb6215c4c53066a56f85df98
charlie-schikula <187229780+CharlieSchikula@users.noreply.github.com>
2024-11-28 16:02:34 +0900

    add gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import "@/styles/globals.css";
 import Layout from "@/components/layout";
+import Script from "next/script";
+import * as gtag from "@/lib/gtag";
 
 // Font Awesomeを使わずにReact iconsを使うことにしたため以下コメントアウト
 // Font Awesome の設定
@@ -8,9 +12,37 @@ import Layout from "@/components/layout";
 // config.autoAddCss = false;
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    }
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script src={"https://www.googletagmanager.com/gtag/js?id={$gtag.GA_MEASUREMENT_ID}"} strategy="afterInteractive" />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag("js", new Date());
+      
+            gtag("config", "${gtag.GA_MEASUREMENT_ID}");
+          `,
+        }} 
+      />
+    
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
